960034114730ad5b70a70e7be3d46f4d02b3134c
FEBIN MATHEW <febinbaby65@gmail.com>
2026-09-24 11:36:11 +0530

    Refactor config.py by removing unused constants
    Removed color definitions and geometry radii constants from config.py.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 
-# Color definitions
 BG_COLOR = "#0b0d13"
 GAUGE_RING = "#171b24"
 BEZEL_DARK = "#232936"
@@ -9,16 +8,13 @@ BEZEL_LIGHT = "#e8edf5"
 RING_BLACK = "#101319"
 DIVIDER_COLOR = "#ccd4e0"
 
-# Official BMW Roundel colors
 BMW_BLUE = "#0066B1"
 BMW_WHITE = "#FFFFFF"
 
-# BMW M-Sport heritage tri-colors
 M_LIGHT_BLUE = "#00a3e0"
 M_DARK_BLUE = "#00205b"
 M_RED = "#e21a21"
 
-# Geometry radii (pixels)
 R_OUTER_BEZEL = 208
 R_OUTER_RIM = 202
 R_INNER_BEZEL = 194
@@ -26,7 +22,6 @@ R_RING_INNER = 120
 R_ROUNDEL = 114
 R_TEXT = 157
 
-# Timing and performance
 FRAME_INTERVAL_MS = 16
 DEFAULT_CRUISE_RPM = 120.0
 SPORT_CRUISE_RPM = 240.0
